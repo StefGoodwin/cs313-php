4bbd7808f44a9cd0c87f7480af06d18a825790a4
Stefnie Goodwin <stefniegoodwin@yahoo.com>
2020-01-17 21:50:37 -0700

    add landing page and assignment page

---
+++ web/cs313_stef/assignments.php
@@ -0,0 +1,41 @@
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <meta charset="utf-8">
+        <title>STEF GOODWIN'S PAGE</title>
+        <link type="text/css" rel="stylesheet" href="/cs313/cs313_stef/styles.css" media="screen">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0"> 
+        
+        
+    </head>
+    
+    <body>
+        <header>
+        
+        
+        </header>
+    
+                
+           
+            <main id="assignments-content">
+                
+            
+                    <h1>Stef's Assignments CS-313</h1>
+                <div class="assignment-box">
+                    <p>COMING SOON!</p>
+                
+                
+                </div>
+                
+                </main>
+               
+    <?php include 'footer.php';?>
+                
+                      
+            
+        
+        
+        
+    </body>
+    
+</html>
---
+++ web/cs313_stef/footer.php
@@ -0,0 +1,7 @@
+<footer class="footer">
+    <div class="foot-con">
+    <p>&copy; 2020 All rights reserved.<br>Last Updated: <?php echo date('l\, F jS Y');?></p>      
+
+     
+    </div>
+</footer>
