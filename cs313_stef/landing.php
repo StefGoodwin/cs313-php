351e3d6010ffcadaafb0e003fa0e13fabc9facdf
Stefnie Goodwin <stefniegoodwin@yahoo.com>
2020-01-17 21:29:49 -0700

    added homepage with a link to assignment page

---
+++ cs313_stef/landing.php
@@ -0,0 +1,58 @@
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <meta charset="utf-8">
+        <title>STEF GOODWIN'S PAGE</title>
+        <link type="text/css" rel="stylesheet" href="/cs313/cs313_stef/styles.css" media="screen">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0"> 
+        
+        
+    </head>
+    
+    <body>
+        <header>
+        
+        
+        </header>
+    
+                
+           
+            <main id="content">
+                
+                
+                <h1>Stef's Landing CS-313</h1>
+                <div class="click">
+                <a target="_blank" href="assignments.php">Click for Assignments</a></div>
+                <div class="container">
+                <div class="gallery">
+                    <a target="_blank" href="images/IMG_0801.JPG">
+                <img src="images/IMG_0801.JPG" class="img1" alt="Sedona Red Rocks"/>
+                </a>
+                     <div class="desc">Paved in Red <br>Sedona, AZ 2020</div>
+                </div>
+                <div class="gallery">
+                    <a target="_blank" href="images/thrutrees.jpg">
+                    <img src="images/thrutrees.jpg" class="img2" alt="Through the Trees"/>
+                    </a>
+                     <div class="desc">Through the Trees <br>Sedona, AZ 2020</div>
+                </div>
+                <div class="gallery">
+                    <a target="_blank" href="images/heavens%20path.jpg">
+                    <img src="images/heavens%20path.jpg" class="img3" alt="Heaven's Path"/>
+                    </a>
+                     <div class="desc">The Crossing <br> Sedona, AZ 2020</div>
+                </div>
+                    
+                </div>
+                </main>
+               
+    <?php include 'footer.php';?>
+                
+                      
+            
+        
+        
+        
+    </body>
+    
+</html>
