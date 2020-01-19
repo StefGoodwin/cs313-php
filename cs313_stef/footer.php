7b3d622c5c46415d33f165b378a507258985b4a8
Stefnie Goodwin <stefniegoodwin@yahoo.com>
2020-01-18 18:53:08 -0700

    updating link to css

--- cs313_stef/footer.php
+++
@@ -1,7 +0,0 @@
-<footer class="footer">
-    <div class="foot-con">
-    <p>&copy; 2020 All rights reserved.<br>Last Updated: <?php echo date('l\, F jS Y');?></p>      
-
-     
-    </div>
-</footer>
