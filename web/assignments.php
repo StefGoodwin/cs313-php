7bf52f3cd48cd7cb08b48d4f0f1d0f3b34a439ab
Stefnie Goodwin <stefniegoodwin@yahoo.com>
2020-02-09 20:56:13 -0700

    test

--- web/assignments.php
+++ web/assignments.php
@@ -4,44 +4,44 @@
         <meta charset="utf-8">
         <title>STEF GOODWIN'S PAGE</title>
         <link type="text/css" rel="stylesheet" href="/styles.css" media="screen">
-        <meta name="viewport" content="width=device-width, initial-scale=1.0"> 
-        
-        
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+
+
     </head>
-    
+
     <body>
         <header>
-        
-        
+
+
         </header>
-    
-                
-           
+
+
+
             <main id="assignments-content">
-                
-            
+
+
                     <h1>Stef's Assignments CS-313</h1>
                 <div class="assignment-box">
                     <ul class="assignments">
 					<li><a href="week03/shopping.php">Week 3 Assignment</a></li>
-					<li><a href="project/artist-form.html">Project</a></li>
-					
-					
-					
+					<li><a href="project/artist-form.php">Project</a></li>
+
+
+
 					</ul>
-                
-                
+
+
                 </div>
-                
+
                 </main>
-               
+
     <?php include 'footer.php';?>
-                
-                      
-            
-        
-        
-        
+
+
+
+
+
+
     </body>
-    
+
 </html>
--- web/project/artist-details.php
+++ web/project/artist-details.php
@@ -7,5 +7,14 @@
   </head>
   <body>
 
+    <?php
+
+foreach ($db->query('SELECT id, artist_name, artist_medium, artist_description FROM artists WHERE artist_name = \'' .  $_POST['artist_name'] . '\';') AS $row) {
+
+    echo '<a href="details.php?id=' . $row['id'] .'">' .  $row['artist_name'] . " " . $row['artist_medium'] . ":" . $row['artist_description'] . '</a>';
+}
+?>
+
+
   </body>
 </html>
