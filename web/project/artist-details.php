999397ee0838f8040e6d78e6159e9cd8555cdc9a
Stefnie Goodwin <stefniegoodwin@yahoo.com>
2020-02-09 21:07:10 -0700

    update

--- web/project/artist-details.php
+++ web/project/artist-details.php
@@ -14,7 +14,7 @@ require('dbConnect.php');
 
 foreach ($db->query('SELECT id, artist_name, artist_medium, artist_description FROM artists WHERE artist_name = \'' .  $_POST['artist_name'] . '\';') AS $row) {
 
-    echo '<a href="details.php?id=' . $row['id'] .'">' .  $row['artist_name'] . " " . $row['artist_medium'] . ":" . $row['artist_description'] . '</a>';
+    echo '<a href="inventory.php?id=' . $row['id'] .'">' .  $row['artist_name'] . " " . $row['artist_medium'] . ":" . $row['artist_description'] . '</a>';
 }
 ?>
 
