f6e0bf20172fca33788ab364c1095cde70ecf65e
Stefnie Goodwin <stefniegoodwin@yahoo.com>
2020-02-14 19:34:41 -0700

    update insert page

--- web/project/insert-artist.php
+++ web/project/insert-artist.php
@@ -20,7 +20,7 @@ echo "item =$item\n";
 echo "merchDescr =$merchDescr\n";
 */
 require("dbConnect.php");
-/*
+
 try
 {
 $query = 'INSERT INTO artists(artist_name, artist_medium, artist_description) VALUES(:artist, :medium, :artDesc)';
@@ -31,8 +31,13 @@ $statement->bindValue(':medium', $medium);
 $statement->bindValue(':artDesc', $artDesc);
 $statement->execute();
 }
+catch (\Exception $ex) {
+  echo "Error with DB. Details: $ex";
+	die();
+}
+
 die();
-*/
+/*
 try
 {
 $query = 'INSERT INTO merchandise(item, merch_size, price, quantity, merch_description) VALUES(:item, :size, :price, :quantity, :merchDescr)';
@@ -50,7 +55,7 @@ $statement->execute();
   echo "Error with DB. Details: $ex";
 	die();
 }
-
+*/
 header("Location: aritst-details.php");
  die();
 
