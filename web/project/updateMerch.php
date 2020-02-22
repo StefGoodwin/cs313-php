112186c05d434c440d54657ce0c4814d8f56fbc4
Stefnie Goodwin <stefniegoodwin@yahoo.com>
2020-02-21 20:34:45 -0700

    update artist project

--- web/project/updateMerch.php
+++ web/project/updateMerch.php
@@ -1,10 +1,10 @@
 <?php
-/*
+
 error_reporting(E_ALL);
 ini_set('display_errors', 1);
 echo '<pre>' , var_dump($_POST) , '</pre>';
 require("dbConnect.php");
-*/
+
 $artistId = $_POST['artistId'];
 $item = $_POST['merchItem'];
 $size = $_POST['merchSize'];
@@ -14,7 +14,7 @@ $item = $_POST['merchItem'];
 $merchDescr = $_POST['merchDescr'];
 
 try {
-$statement = 'UPDATE merchandise SET item = :item, merch_size = :size, price = :price, quantity = :quantity + 5, merch_description = :merchDescr WHERE artist_id = :artist_id';
+$statement = 'UPDATE merchandise SET artist_id = :artist_id, item = :item, merch_size = :size, price = :price, quantity = :quantity, merch_description = :merchDescr WHERE id = :id';
 $statement = $db->prepare($statement);
 
 $statement->bindValue(':artist_id', $artistId);
@@ -23,6 +23,7 @@ $statement->bindValue(':size', $size);
 $statement->bindValue(':price', $price);
 $statement->bindValue(':quantity', $quantity);
 $statement->bindValue(':merchDescr', $merchDescr);
+$statement->bindValue(':id', $id);
 $statement->execute();
 
 }
@@ -30,8 +31,8 @@ catch (PDOException $ex)
 {
 	echo "Error with DB. Details: $ex";
 }
-
+/*
 header('location: artist-details.php');
  die();
-
+*/
  ?>
